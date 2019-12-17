58c6004666ec48ba16de0d1a918284285e2a61d1
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-17 11:41:26 -0700

    Added apiCalls file to get all the movies returned.  On page load, implemented so all the movies data are put in redux store.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchAllMovies } from '../apiCalls';
+import { addMovies } from '../actions/index';
 
 export class App extends Component {
+  componentDidMount() {
+    fetchAllMovies().then(data => this.props.addMovies(data.movies));
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +17,8 @@ export class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  addMovies: movies => dispatch(addMovies(movies))
+});
+
+export default connect(null, mapDispatchToProps)(App);
