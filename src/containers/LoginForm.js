3f141fdc6c8c901e27fc15fe223a75cbb9cfb2c3
Alan Turing <austeyd@gmail.com>
2019-12-17 18:58:33 -0700

    Add LoginForm JSX

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
   render() {
     return (
-      <div>
+      <section className="login-section">
+        <form>
+          <div className="input-container">
+            <label for="email-input">Email:</label>
+            <input id="email-input" type="text" placeholder="Email Address" value={this.state.email} name="email" />
+          </div>
+          <div className="input-container">
+            <label for="password-input">Password:</label>
+            <input id="password-input" type="text" placeholder="Password" value={this.state.password} name="password" />
+          </div>
+        </form>
         <h1>LoginForm</h1>
       </div>
     );
