4ea1a93446a4bb3d68f6f6ef701289c35986dba9
Alan Turing <austeyd@gmail.com>
2019-12-17 18:59:24 -0700

    Add handleInputChange method to LoginForm component

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -10,17 +10,21 @@ class LoginForm extends Component {
     }
   }
 
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
     return (
       <section className="login-section">
         <form>
           <div className="input-container">
             <label for="email-input">Email:</label>
-            <input id="email-input" type="text" placeholder="Email Address" value={this.state.email} name="email" />
+            <input id="email-input" type="text" placeholder="Email Address" value={this.state.email} name="email" onChange={event => this.handleInputChange(event)} />
           </div>
           <div className="input-container">
             <label for="password-input">Password:</label>
-            <input id="password-input" type="text" placeholder="Password" value={this.state.password} name="password" />
+            <input id="password-input" type="text" placeholder="Password" value={this.state.password} name="password" onChange={event => this.handleInputChange(event)} />
           </div>
         </form>
         <h1>LoginForm</h1>
