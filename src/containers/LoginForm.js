58836fce2a4946bb598e0ce05ec6750f81febf0c
Alan Turing <austeyd@gmail.com>
2019-12-17 19:38:48 -0700

    Add placeholder styling for login page

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './LoginForm.css'
 
 class LoginForm extends Component {
   constructor() {
@@ -17,18 +18,20 @@ class LoginForm extends Component {
   render() {
     return (
       <section className="login-section">
-        <h1>Login Form</h1>
-        <form>
-          <div className="input-container">
-            <label for="email-input">Email:</label>
-            <input id="email-input" type="text" placeholder="Email Address" value={this.state.email} name="email" onChange={event => this.handleInputChange(event)} />
-          </div>
-          <div className="input-container">
-            <label for="password-input">Password:</label>
-            <input id="password-input" type="text" placeholder="Password" value={this.state.password} name="password" onChange={event => this.handleInputChange(event)} />
-          </div>
-          <button className="submit-btn"></button>
-        </form>
+        <div className="form-container">
+          <h1>Login Form</h1>
+          <form>
+            <div className="input-container">
+              <label for="email-input">Email:</label>
+              <input id="email-input" type="text" placeholder="Email Address" value={this.state.email} name="email" onChange={event => this.handleInputChange(event)} />
+            </div>
+            <div className="input-container">
+              <label for="password-input">Password:</label>
+              <input id="password-input" type="text" placeholder="Password" value={this.state.password} name="password" onChange={event => this.handleInputChange(event)} />
+            </div>
+            <button className="submit-btn">Submit</button>
+          </form>
+        </div>
       </section>
     );
   }
