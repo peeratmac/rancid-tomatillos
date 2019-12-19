556edf76b03e4d85911c858adb35534e02a945e1
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-18 20:30:19 -0700

    Display movie information specific to the movie selected for route /movies/:id

--- src/components/MovieShowPage.js
+++ src/components/MovieShowPage.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const MovieShowPage = props => {
-  console.log(props);
+  const {
+    id,
+    title,
+    backdrop_path,
+    release_date,
+    overview,
+    average_rating
+  } = props;
+
   return (
     <div>
-      <h1>x</h1>
+      <img className='backdrop' src={backdrop_path} alt={title} />
+      <h1>{title}</h1>
+      <p>{release_date}</p>
+      <p>{overview}</p>
+      <p>Average Rating: {average_rating}</p>
     </div>
   );
 };
