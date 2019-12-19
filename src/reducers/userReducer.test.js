759986b9aaddb657ab9ab6b500843f0f4dbcead5
Alan Turing <austeyd@gmail.com>
2019-12-19 11:14:05 -0700

    Add userReducer tests

---
+++ src/reducers/userReducer.test.js
@@ -0,0 +1,22 @@
+import { userReducer } from '../reducers/userReducer';
+
+describe('userReducer', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+    const result = userReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the correct state if the action is UPDATE_USER', () => {
+    const initialState = {id: '', name: '', email: ''};
+    const action = {
+      type: 'UPDATE_USER',
+      user: {id: 1, name: 'placeholder', email: 'placeholder'}
+    };
+    const result = userReducer(initialState, action);
+    const expectedState = {id: 1, name: 'placeholder', email: 'placeholder'};
+
+    expect(result).toEqual(expectedState);
+  });
+});
