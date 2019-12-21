8598d3eebae27dbcd6347c7a2b2c9390f939ab89
Alan Turing <austeyd@gmail.com>
2019-12-21 12:59:19 -0700

    Correct element className typos

--- src/components/MovieShowPage.js
+++ src/components/MovieShowPage.js
@@ -20,19 +20,19 @@ export const MovieShowPage = props => {
       <p>In Theaters: {release_date}</p>
       <p className="overview">{overview}</p>
       <p>Average Rating: {average_rating}</p>
-      <div class="rating-bar">
-        <p class="rating-description">Rate Film:</p>
-        <div class="rating-scale">
-          <button class="rating-btn btn-one" value="1">1</button>
-          <button class="rating-btn btn-two" value="2">2</button>
-          <button class="rating-btn btn-three" value="3">3</button>
-          <button class="rating-btn btn-four" value="4">4</button>
-          <button class="rating-btn btn-five" value="5">5</button>
-          <button class="rating-btn btn-six" value="6">6</button>
-          <button class="rating-btn btn-seven" value="7">7</button>
-          <button class="rating-btn btn-eight" value="8">8</button>
-          <button class="rating-btn btn-nine" value="9">9</button>
-          <button class="rating-btn btn-ten" value="10">10</button>
+      <div className="rating-bar">
+        <p className="rating-description">Rate Film:</p>
+        <div className="rating-scale">
+          <button className="rating-btn btn-one" value="1">1</button>
+          <button className="rating-btn btn-two" value="2">2</button>
+          <button className="rating-btn btn-three" value="3">3</button>
+          <button className="rating-btn btn-four" value="4">4</button>
+          <button className="rating-btn btn-five" value="5">5</button>
+          <button className="rating-btn btn-six" value="6">6</button>
+          <button className="rating-btn btn-seven" value="7">7</button>
+          <button className="rating-btn btn-eight" value="8">8</button>
+          <button className="rating-btn btn-nine" value="9">9</button>
+          <button className="rating-btn btn-ten" value="10">10</button>
         </div>
       </div>
     </div>
