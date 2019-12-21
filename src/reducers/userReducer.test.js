44d573d867c2a5137190747e6d276efb43bbee86
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-21 14:00:02 -0700

    Update tests to reflect changes

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -2,20 +2,21 @@ import { userReducer } from '../reducers/userReducer';
 
 describe('userReducer', () => {
   it('should return the initial state', () => {
-    const expected = {id: '', name: '', email: ''};
+    const expected = {};
     const result = userReducer(undefined, {});
 
     expect(result).toEqual(expected);
   });
 
   it('should return the correct state if the action is UPDATE_USER', () => {
-    const initialState = {id: '', name: '', email: ''};
+    const initialState = {};
     const action = {
       type: 'UPDATE_USER',
-      user: {id: 1, name: 'placeholder', email: 'placeholder'}
+      user: {id: 1, name: 'placeholder', email: 'placeholder', ratings: []}
     };
     const result = userReducer(initialState, action);
-    const expectedState = {id: 1, name: 'placeholder', email: 'placeholder'};
+    const expectedState = {id: 1, name: 'placeholder',
+      email: 'placeholder', ratings: []};
 
     expect(result).toEqual(expectedState);
   });
