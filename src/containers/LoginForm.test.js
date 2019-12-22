8119534020427d01444b3ec6980cb73762044b6d
Alan Turing <austeyd@gmail.com>
2019-12-22 13:15:46 -0700

    Add mapToPropTests for LoginForm

--- src/containers/LoginForm.test.js
+++ src/containers/LoginForm.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LoginForm } from './LoginForm';
+import { LoginForm, mapStateToProps, mapDispatchToProps } from './LoginForm';
+import { handleError, updateLoggedInStatus, updateUser } from '../actions';
 
 describe('LoginForm', () => {
   let wrapper;
@@ -9,5 +10,59 @@ describe('LoginForm', () => {
     wrapper = shallow(<LoginForm />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with error and loggedIn properties from state', () => {
+      const mockState = {
+        movies: [],
+        user: {},
+        isLoggedIn: true,
+        errorMessage: '',
+        loadingStatus: false,
+      };
+      const expected = {
+        isLoggedIn: true,
+        errorMessage: ''
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with a handleError action when handleInputChange is called', () => {
+      // Setup
+      const mockDispatch = jest.fn();
+      const actionToDispatch = handleError('mockError');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.handleError('mockError');
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('calls dispatch with a updateUser action when handleInputChange is called', () => {
+      // Setup
+      const mockDispatch = jest.fn();
+      const actionToDispatch = updateUser({mock: 'property'});
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.updateUser({mock: 'property'});
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('calls dispatch with a updateLoggedInStatus action when handleInputChange is called', () => {
+      // Setup
+      const mockDispatch = jest.fn();
+      const actionToDispatch = updateLoggedInStatus(true);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.updateLoggedInStatus(true);
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
 });
