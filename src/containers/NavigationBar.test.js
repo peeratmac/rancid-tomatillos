b8ae1cc181943ed5b86eb0ff93c1aede2cab6eb8
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-23 11:30:54 -0700

    Add unit test for updateUser invocation on click

--- src/containers/NavigationBar.test.js
+++ src/containers/NavigationBar.test.js
@@ -26,6 +26,26 @@ describe('NavigationBar', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
-  
 
+  it('should invoke updateUser props when log out button is clicked',
+    () => {
+      const mockUpdateUser = jest.fn();
+      wrapper = shallow(<NavigationBar
+        user={{ id: 22, name: 'Marge', email: 'marge@turing.io', ranking: [] }}
+        isLoggedIn={true}
+        updateUser={mockUpdateUser}
+        updateLoggedInStatus={jest.fn()}
+        />);
+
+      wrapper.find('.logout-button').simulate('click');
+      expect(mockUpdateUser).toHaveBeenCalledWith({});
+  });
+
+  // it('should invoke updateLoggedInStatus props with status passed in when button is clicked',
+  //   () => {
+  //
+  // });
+
+  // without Redux we would test that updateUser and updateLoggedInStatus update
+  // state (which is the Redux store).  With Redux, do we still test this? how?
 });
