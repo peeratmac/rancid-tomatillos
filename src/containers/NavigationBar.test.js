282a7652c9dabf8a1959144007604a78cca0739a
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-23 11:39:15 -0700

    Refactor tests to use beforeEach for cleaner code

--- src/containers/NavigationBar.test.js
+++ src/containers/NavigationBar.test.js
@@ -4,6 +4,23 @@ import { NavigationBar } from './NavigationBar';
 
 describe('NavigationBar', () => {
   let wrapper;
+  let mockUpdateUser;
+  let mockUpdateLoggedInStatus;
+
+  beforeEach(() => {
+    mockUpdateUser = jest.fn();
+    mockUpdateLoggedInStatus = jest.fn();
+    wrapper = shallow(<NavigationBar
+      user={{ id: 22, name: 'Marge', email: 'marge@turing.io', ranking: [] }}
+      isLoggedIn={true}
+      updateUser={mockUpdateUser}
+      updateLoggedInStatus={mockUpdateLoggedInStatus}
+      />);
+  })
+
+  it('should match snapshot when user is logged in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
   it('should match the NavigationBar Snapshot when no one is logged in', () => {
     wrapper = shallow(<NavigationBar
@@ -16,45 +33,16 @@ describe('NavigationBar', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should match snapshot when user is logged in', () => {
-    wrapper = shallow(<NavigationBar
-      user={{ id: 22, name: 'Marge', email: 'marge@turing.io', ranking: [] }}
-      isLoggedIn={true}
-      updateUser={jest.fn()}
-      updateLoggedInStatus={jest.fn()}
-      />);
-
-    expect(wrapper).toMatchSnapshot();
-  });
-
   it('should invoke updateUser props with empty object when log out button is clicked',
     () => {
-      const mockUpdateUser = jest.fn();
-      wrapper = shallow(<NavigationBar
-        user={{ id: 22, name: 'Marge', email: 'marge@turing.io', ranking: [] }}
-        isLoggedIn={true}
-        updateUser={mockUpdateUser}
-        updateLoggedInStatus={jest.fn()}
-        />);
-
       wrapper.find('.logout-button').simulate('click');
       expect(mockUpdateUser).toHaveBeenCalledWith({});
   });
 
   it('should invoke updateLoggedInStatus props with false status when button is clicked',
     () => {
-      const mockUpdateLoggedInStatus = jest.fn();
-      wrapper = shallow(<NavigationBar
-        user={{ id: 22, name: 'Marge', email: 'marge@turing.io', ranking: [] }}
-        isLoggedIn={true}
-        updateUser={jest.fn()}
-        updateLoggedInStatus={mockUpdateLoggedInStatus}
-        />);
-
       wrapper.find('.logout-button').simulate('click');
       expect(mockUpdateLoggedInStatus).toHaveBeenCalledWith(false);
   });
 
-  // without Redux we would test that updateUser and updateLoggedInStatus update
-  // state (which is the Redux store).  With Redux, do we still test this? how?
 });
