8ded167ec2de441a2b520706ea4468cb1266fd5b
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-23 11:11:59 -0700

    Ad d second snapshot for NavBar for conditional rendering based on logged in status

--- src/containers/NavigationBar.test.js
+++ src/containers/NavigationBar.test.js
@@ -5,9 +5,27 @@ import { NavigationBar } from './NavigationBar';
 describe('NavigationBar', () => {
   let wrapper;
 
-  it('should match the NavigationBar Snapshot', () => {
-    wrapper = shallow(<NavigationBar />);
+  it('should match the NavigationBar Snapshot when no one is logged in', () => {
+    wrapper = shallow(<NavigationBar
+        user={{}}
+        isLoggedIn={false}
+        updateUser={jest.fn()}
+        updateLoggedInStatus={jest.fn()}
+      />);
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should match snapshot when user is logged in', () => {
+    wrapper = shallow(<NavigationBar
+      user={{ id: 22, name: 'Marge', email: 'marge@turing.io', ranking: [] }}
+      isLoggedIn={true}
+      updateUser={jest.fn()}
+      updateLoggedInStatus={jest.fn()}
+      />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+
 });
