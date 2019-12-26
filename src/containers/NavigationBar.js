72ba780c67b1389040f02d0709f61c430f3e1781
Alan Turing <austeyd@gmail.com>
2019-12-26 13:07:03 -0700

    Fix error message persistence across pages

--- src/containers/NavigationBar.js
+++ src/containers/NavigationBar.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Link } from 'react-router-dom';
 import './NavigationBar.css';
-import { updateUser, updateLoggedInStatus } from '../actions/index';
+import { updateUser, updateLoggedInStatus, handleError } from '../actions/index';
 import PropTypes from 'prop-types';
 
 export const NavigationBar = props => {
-  const { user, isLoggedIn, updateUser, updateLoggedInStatus } = props;
+  const { user, isLoggedIn, updateUser, updateLoggedInStatus, handleError } = props;
 
   return (
     <header className='header'>
       <Link to='/'>
         <Route
           path={['/login', '/movies/:id']}
-          render={() => <button className='home-button'>Home</button>}
+          render={() =>
+            <button
+              className='home-button'
+              onClick={() => {
+                handleError('')
+              }}>
+              Home
+            </button>}
         />
       </Link>
       {isLoggedIn && <h1 className='welcome-user'>Welcome {user.name}</h1>}
@@ -45,7 +52,8 @@ export const mapStateToProps = state => ({
 
 export const mapDispatchToProps = dispatch => ({
   updateUser: user => dispatch(updateUser(user)),
-  updateLoggedInStatus: status => dispatch(updateLoggedInStatus(status))
+  updateLoggedInStatus: status => dispatch(updateLoggedInStatus(status)),
+  handleError: errorMessage => dispatch(handleError(errorMessage))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar);
