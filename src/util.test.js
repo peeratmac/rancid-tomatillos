9d0f1a3c563a0489bea19c4c0f455ea37a83c375
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-04 21:11:56 -0700

    Add unit test for findRatings to return correct score

--- src/util.test.js
+++ src/util.test.js
@@ -16,16 +16,35 @@ describe('Util', () => {
         updated_at:"2019-12-25T20:16:34.893Z"},
       {id:46,
         user_id:9,
-        movie_id:2,
+        movie_id:3,
         rating:10,
         created_at:"2019-12-25T20:30:21.606Z",
         updated_at:"2019-12-25T20:30:21.606Z"}
       ]}
   })
 
-  it('should return \'...\' when user has not yet rated movie', () => {
-    let expected = '...';
-
+  it('should return correct rating score when passed the correct arguments',
+    () => {
+    let expected = 10;
     expect(findRating(mockId, mockUser, 'rating')).toEqual(expected);
-  })
+  });
+
+  it('should return \'...\' when user has not yet rated movie', () => {
+    mockUser = {id: 9, name: 'Marge', email: 'marge@turing.io', ratings: [
+      {id:45,
+        user_id:9,
+        movie_id:1,
+        rating:8,
+        created_at:"2019-12-25T20:16:34.893Z",
+        updated_at:"2019-12-25T20:16:34.893Z"},
+      {id:46,
+        user_id:9,
+        movie_id:2,
+        rating:10,
+        created_at:"2019-12-25T20:30:21.606Z",
+        updated_at:"2019-12-25T20:30:21.606Z"}
+      ]};
+    let expected = '...';
+    expect(findRating(mockId, mockUser, 'rating')).toEqual(expected);
+  });
 })
