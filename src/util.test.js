a0c96587a156f22033b64980a26f43519153c6be
Alan Turing <austeyd@gmail.com>
2020-01-06 17:23:07 -0700

    Add sortFilms test to util file

--- src/util.test.js
+++ src/util.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {findRating} from './util';
+import { findRating, sortFilms} from './util';
 
 describe('Util', () => {
   let mockId;
@@ -52,5 +52,62 @@ describe('Util', () => {
       ]};
     let expected = '...';
     expect(findRating(mockId, mockUser, 'rating')).toEqual(expected);
+  });
+
+  it('should return an array of sorted movies when passed an array of films', () => {
+    const mockMoviesData = [
+      {
+        id: 2,
+        title: 'Ad Astra',
+        poster_path:
+          'https://image.tmdb.org/t/p/original//xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg',
+        backdrop_path:
+          'https://image.tmdb.org/t/p/original//5BwqwxMEjeFtdknRV792Svo0K1v.jpg',
+        release_date: '2019-09-17',
+        overview:
+          'The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet Earth, astronaut Roy McBride undertakes a mission across the immensity of space and its many perils to uncover the truth about a lost expedition that decades before boldly faced emptiness and silence in search of the unknown.',
+        average_rating: 7
+      },
+      {
+        id: 1,
+        title: 'Jumanji: The Next Level',
+        poster_path:
+          'https://image.tmdb.org/t/p/original//l4iknLOenijaB85Zyb5SxH1gGz8.jpg',
+        backdrop_path:
+          'https://image.tmdb.org/t/p/original//zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg',
+        release_date: '2019-12-04',
+        overview:
+          "In Jumanji: The Next Level, the gang is back but the game has changed. As they return to rescue one of their own, the players will have to brave parts unknown from arid deserts to snowy mountains, to escape the world's most dangerous game.",
+        average_rating: 4
+      }
+    ];
+    let expected = [
+      {
+        id: 1,
+        title: 'Jumanji: The Next Level',
+        poster_path:
+          'https://image.tmdb.org/t/p/original//l4iknLOenijaB85Zyb5SxH1gGz8.jpg',
+        backdrop_path:
+          'https://image.tmdb.org/t/p/original//zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg',
+        release_date: '2019-12-04',
+        overview:
+          "In Jumanji: The Next Level, the gang is back but the game has changed. As they return to rescue one of their own, the players will have to brave parts unknown from arid deserts to snowy mountains, to escape the world's most dangerous game.",
+        average_rating: 4
+      },
+      {
+        id: 2,
+        title: 'Ad Astra',
+        poster_path:
+          'https://image.tmdb.org/t/p/original//xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg',
+        backdrop_path:
+          'https://image.tmdb.org/t/p/original//5BwqwxMEjeFtdknRV792Svo0K1v.jpg',
+        release_date: '2019-09-17',
+        overview:
+          'The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet Earth, astronaut Roy McBride undertakes a mission across the immensity of space and its many perils to uncover the truth about a lost expedition that decades before boldly faced emptiness and silence in search of the unknown.',
+        average_rating: 7
+      }
+    ];
+
+    expect(sortFilms(mockMoviesData)).toEqual(expected);
   });
 })
